e6eab0ac594b4281aecf7882ade8bd4094661664
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-09-07 23:53:45 -0400

    Extract Matches using .match() It will return the matched word instead of true or false like .test()

--- regEx.js
+++ regEx.js
@@ -73,3 +73,23 @@ Write a regex fccRegex to match "freeCodeCamp", no matter its case. Your regex s
 let myString = "freeCodeCamp";
 let fccRegex = /freecodecamp/i; // Change this line
 let result = fccRegex.test(myString);
+
+// Extract Matches
+/*
+So far, you have only been checking if a pattern exists or not within a string. You can also extract the actual matches you found with the .match() method.
+
+To use the .match() method, apply the method on a string and pass in the regex inside the parentheses. Here's an example:
+
+"Hello, World!".match(/Hello/);
+// Returns ["Hello"]
+let ourStr = "Regular expressions";
+let ourRegex = /expressions/;
+ourStr.match(ourRegex);
+// Returns ["expressions"]
+
+Apply the .match() method to extract the word coding.
+*/
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
